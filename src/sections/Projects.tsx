29c909219bc0952c625039f333fdab36052cae09
Glax3max <130317166+Glax3max@users.noreply.github.com>
2025-01-30 18:23:12 +0530

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,10 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
-import grainImage from "@/assets/images/grain.jpg"
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 const portfolioProjects = [
@@ -56,7 +53,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project,projectIndex) => (
             <Card 
             className=" md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:rounded-3xl pb-0 after:outline-white/20 px-8 pt-8 sticky top-16"
-            key={project.title} 
+            key={projectIndex} 
             style={{
               top:`calc(64px + ${projectIndex * 80}px)`
             }}
@@ -75,8 +72,8 @@ export const ProjectsSection = () => {
                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
                 <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
                 <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                  {project.results.map((result) => (
-                    <li className="flex gap-2 text-sm md:text-base  text-white/50">
+                  {project.results.map((result,resultIndex) => (
+                    <li key={resultIndex} className="flex gap-2 text-sm md:text-base  text-white/50">
                       <Image src={CheckCircleIcon} className="size-5 md:size-6" alt="circle"/>
                       <span>
                         {result.title}
